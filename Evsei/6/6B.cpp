0e2853f66f2734847c8c287de609dfb30f5d1182
Irina Evsei <irinaesei@mail.ru>
2015-12-09 20:16:03 +0200

    Few changes

--- Evsei/6/6B.cpp
+++ Evsei/6/6B.cpp
@@ -5,18 +5,18 @@ using namespace std;
 //печать массива
 void initArray(int[], int);
 void printArray(int[], int);
-//удал¤ем N-ый элемент массива (сдвига¤ остальные элементы)
+//удаляем N-ый элемент массива (сдвигая остальные элементы)
 void deleteValue(int[], int, int);
 const int N = 10;
 int main()
 {
 	setlocale(LC_ALL, "Russian");
 	int array[N], n = 0;
-	//инициализаци¤ массива
+	//инициализация массива
 	initArray(array, N);
 	//печать массива
 	printArray(array, N);
-	//удал¤ем N-ый элемент массива (сдвига¤ остальные элементы)
+	//удаляем N-ый элемент массива (сдвигая остальные элементы)
 	deleteValue(array, N, n);
 	//печать нового массива
 	printArray(array, N);
@@ -24,7 +24,7 @@ int main()
 
 void initArray(int array[], int N)
 {
-	cout << "¬ведите элементы массива: " << endl;
+	cout << "введите элементы массива: " << endl;
 	for (int i = 0; i < N; i++){
 		cin >> array[i];
 	}
@@ -43,7 +43,7 @@ void deleteValue(int array[], int N, int n)
 {
 	while (n<0 || n>N){
 	}
-	cout << "¬ведите номер элемента, который нужно удалить : ";
+	cout << "введите номер элемента, который нужно удалить : ";
 	cin >> n;
 	for (int i = n; i < N - 1; i++){
 		array[i] = array[i + 1];
