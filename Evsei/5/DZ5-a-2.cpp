311bc1f6b8011dd509ca77facf760b1d4af31d8c
Irina Evsei <irinaesei@mail.ru>
2015-12-12 22:27:22 +0200

    Добавлен поиск седловой точки

--- Evsei/5/DZ5-a-2.cpp
+++ Evsei/5/DZ5-a-2.cpp
@@ -9,11 +9,11 @@ const int M = 5;
 void initArray(int[][M], int, int);
 void printArray(int [][M], int, int);
 //найти седловую точку если она там есть. Седловой точкой называется элемент матрицы который является минимумом в строке и максимумом в столбце.
-
+void findSedlPoint(int array[][M], int);
 //Вывести сумму элементов главной диагонали матрицы(N = M)
-void sumOfMainDiagonal(int array[][M], int N);
+void sumOfMainDiagonal(int array[][M], int );
 //Вывести сумму элементов нижнего треугольника матрицы(N = M)
-void sumOFFooterTriangle(int array[][M], int N);
+void sumOFFooterTriangle(int array[][M], int);
 
 
 int main()
@@ -22,6 +22,8 @@ int main()
 	int array[N][M];
 	initArray(array, N, M);
 	printArray(array, N, M);
+	//найти седловую точку если она там есть.
+	findSedlPoint(array, N);
 	//Вывести сумму элементов главной диагонали матрицы(N = M)
 	sumOfMainDiagonal(array, N);
 	//Вывести сумму элементов нижнего треугольника матрицы(N = M)
@@ -77,5 +79,41 @@ void sumOFFooterTriangle(int array[][M], int N)
 		}
 	}
 	cout << "сумма нижнего треугольника исключая диагональ равна " << sum << endl;
+}
+
+void findSedlPoint(int array[][M], int N)
+{
+	bool foundSedlPoint = false;
+	for (int i = 0; i < N; i++){
+		
+		//Находим минимальный элемент в i строке
+		int minValueInRow = array[i][0];
+		int minPozInRow = 0;
+		for (int j = 0; j < M; j++){
+			if (array[i][j] < minValueInRow){
+				minValueInRow = array[i][j];
+				minPozInRow = j;
+			}
+		}
+
+		//Находим максимальный элемент в minPozInRow стобце
+		int maxValueInCol = array[i][minPozInRow];
+		bool foundGreater = false;
+		for (int k = 0; k < N; k++){
+			if (array[k][minPozInRow] > maxValueInCol){
+				foundGreater = true;
+				break;
+			}
+		}
+
+		if (!foundGreater){
+			foundSedlPoint = true;
+			cout << "Седловая точка была найдена в " << i << " строке, в " << minPozInRow << " стоблце. Число:" << array[i][minPozInRow] << endl;
+		}
+	}
+
+	if (!foundSedlPoint){
+		cout << "Седловых точек не было найдено"<< endl;
+	}
 }
 
