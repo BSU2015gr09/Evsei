7a2879de271fd959b1d8304fe13a0a0e755971f5
Irina Evsei <irinaesei@mail.ru>
2016-03-22 00:05:09 +0200

    Fstream for task 3

--- Evsei/2 семестр/3DZ 37.cpp
+++ Evsei/2 семестр/3DZ 37.cpp
@@ -3,6 +3,7 @@
 #include <iostream> 
 #include <cstring>
 #include <clocale> 
+#include <fstream>
 
 using namespace std;
 //ввод строки
@@ -11,7 +12,7 @@ void enterString(char*, short);
 bool revAlphOrder(char*);
 
 int main(){
-	const short n = 256;
+	const int n = 1000;
 	setlocale(LC_ALL, "Russian");
 	char *str = new char[n];
 	//ввод строки
@@ -20,7 +21,7 @@ int main(){
 	char *p = strtok(str, " ,.-;:");
 	while (p){
 		if (revAlphOrder(p)){
-			cout << "Порядок обратный алфавитному у [ " <<p << " ]"<< endl;
+			cout << "Порядок обратный алфавитному у [ " << p << " ]" << endl;
 		}
 		p = strtok(NULL, " ,.-;:");
 	}
@@ -31,23 +32,30 @@ int main(){
 //ввод строки
 void enterString(char * str, short n){
 	int k = 0;
-	cout << "Выберите действие:" << endl << "  1. Автоматический ввод строки. " << endl << "  2. Ввод строки вручную. " << endl;
+	cout << "Выберите действие:" << endl << "  1. Автоматический ввод строки. " << endl << "  2. Ввод строки вручную. " << endl << "  3. Из текстового файла" << endl;
 	cin >> k;
 	switch (k){
 	case 1: strcpy(str, "Почти весна, чуть-чуть мешает метр снега.");
 		cout << str << endl;
 		break;
-	default:
-		cout << "Введите строку\n";
+	case 2:
+		cout << "Введите строку" << "\n";
 		cin.get();
 		cin.getline(str, n);
+		break;
+	default:
+		ifstream infile("D:\\text.txt");
+		while (!infile.eof()){
+			infile.getline(str, n);
+		}
+		break;
 	}
 }
 
 //проверяем массив на нужную нам сортировку
 bool revAlphOrder(char * str){
 	while (*(++str)){
-		if (*(str - 1) < *(str)) 
+		if (*(str - 1) < *(str))
 			return false;
 	}
 	return true;
